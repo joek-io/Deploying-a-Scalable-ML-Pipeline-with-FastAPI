544e2698b80dd247c8029b15b15b8b4e43e7edb0
Joseph Kuykendall <56773616+joek-io@users.noreply.github.com>
2025-10-05 22:25:19 -0400

    Adding TODO statements to local_api.py

--- local_api.py
+++ local_api.py
@@ -2,15 +2,11 @@ import json
 
 import requests
 
-# TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
+r = requests.get("http://127.0.0.1:8000/")
 
-# TODO: print the status code
-# print()
-# TODO: print the welcome message
-# print()
+print(r.status_code)
 
-
+print(r.json().get("message"))
 
 data = {
     "age": 37,
@@ -29,10 +25,7 @@ data = {
     "native-country": "United-States",
 }
 
-# TODO: send a POST using the data above
-r = None # Your code here
+r = requests.post("http://127.0.0.1:8000/data/", json=data)
 
-# TODO: print the status code
-# print()
-# TODO: print the result
-# print()
+print(r.status_code)
+print(r.json().get("result"))
